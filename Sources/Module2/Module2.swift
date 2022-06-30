3fcdc07289470b85c63125dc0de53202d104c1b7
Tero <tjankko@hotmail.com>
2022-06-30 06:56:39 -0700

    Initial commit

---
+++ Sources/Module2/Module2.swift
@@ -0,0 +1,6 @@
+public struct Module2 {
+    public private(set) var text = "Hello, World! - from Module2"
+
+    public init() {
+    }
+}
---
+++ Tests/Module2Tests/Module2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Module2
+
+final class Module2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Module2().text, "Hello, World!")
+    }
+}
